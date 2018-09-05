6c9de387491adc9af73ee20e91a437f50fc2e576
ACH <andresch@gmail.com>
2018-09-05 11:28:28 -0600

    hh

--- CeatSheet/CCMPcode.py
+++ CeatSheet/CCMPcode.py
@@ -1,20 +1,26 @@
 import code
 
 class CCMPcode:
-    """description of class 77"""
+    """description of class 6-6"""
         
     name = ""
     category = ""
     pcode = 0
 
-    def __init__(self, name, category, pcode):
+    def __init__(self, name, category, pcode1):
           print("This is the constructor method.")
           self.name = name
           self.category = category
-          self.pcode = pcode
+          self.pcode = pcode1
 
 
     def displayCCMPcode(self):
+          print("Name : ", self.name,  ", Category: ", self.category ,", Code: ", self.pcode)
+
+    def displayCCMPcode1(self):
+          print("Name : ", self.name,  ", Category: ", self.category ,", Code: ", self.pcode)
+
+    def displayCCMPcode3(self):
           print("Name : ", self.name,  ", Category: ", self.category ,", Code: ", self.pcode)
 
 
