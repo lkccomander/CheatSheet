2e2a23a8cc7423ea41efb2a0f1cef6b633983c2c
Duke <lkccomander@gmail.com>
2018-09-06 16:21:22 -0600

    List 2 Util json

--- CeatSheet/Conn.py
+++ CeatSheet/Conn.py
@@ -2,11 +2,17 @@
 from pymongo import MongoClient
 from UtilService import UtilService
 import pymongo
-import json
+import UtilService
 from bson import BSON
 from bson import json_util
+import json
 import random
 import code
+import io
+try:
+    to_unicode = unicode
+except NameError:
+    to_unicode = str
 
 
 class Conn():
@@ -28,21 +34,46 @@ class Conn():
         self1 = pymongo.MongoClient("mongodb+srv://userMongo4:123admina@cluster0-0njxn.mongodb.net/test?retryWrites=true")
         mydb = self1.CheatSheet
         mycol = mydb.CheatSheet
-        single_doc = mycol.find().sort("name", -1)
+        json_docs = []
+        single_doc = mycol.find().sort("code", -1)
+        new_dict = mycol.find().sort("code", -1)
         
-        for x in single_doc:
-            print(x)
+        #for x in single_doc:
+            #print(x)
         print("-------------------------------KAROL La Niña de las flores-5--------------------------------")
 
+       
+        #https://stackoverflow.com/questions/19674311/json-serializing-mongodb
+
+        for doc in new_dict:
+            json_doc = json.dumps(doc, default=json_util.default)
+            #json_doc = json_doc.replace("$oid", "id")            #json_doc = json_doc.replace("_id", "uid")
+            #print("---------------------------------------------------------------")
+            #print(doc)
+            json_docs.append(json_doc)
+        
+            
+        print(json_docs)
+
+
+        with open('data1.json', 'w') as outfile:
+          json.dump(json_docs, outfile, sort_keys = True, indent = 4,ensure_ascii = False)
+        
+        # Write JSON file
+        with io.open('data.json', 'w', encoding='utf8') as outfile:
+          str_ = json.dumps(json_docs,indent=4, sort_keys=True,separators=(',', ': '), ensure_ascii=False)
+          outfile.write(to_unicode(str_))
+        #Check https://github.com/MartinThoma/mpu
         
 
-        print("-------------------------------KAROL La Niña de las flores-5--------------------------------")
-        json_doc = json.dumps(single_doc,default=json_util.default)
-        json_doc = json_doc.replace("$oid", "id")
-        json_doc = json_doc.replace("_id", "uid")
+
         print("-------------------------------KAROL La Niña de las flores-6--------------------------------")
-        print(json_doc)
-        return json.loads(str(json_doc))
+        #json_doc = json.dumps(single_doc,default=json_util.default)
+        #json_doc = json_doc.replace("$oid", "id")
+        #json_doc = json_doc.replace("_id", "uid")
+        print("-------------------------------KAROL La Niña de las flores-7--------------------------------")
+        #print(json_doc)
+        #return json.loads(str(json_doc))
 #-----------------------------------------------------------------------------------------------------------------------#
          
     def closeConexion(self):
