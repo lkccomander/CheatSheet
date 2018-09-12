b042e93ca5e945d4aa57ba889a16f47479989a64
ACH <andresch@gmail.com>
2018-09-12 06:43:55 -0600

    Changes at the office

--- CeatSheet/Conn.py
+++ CeatSheet/Conn.py
@@ -37,14 +37,10 @@ class Conn():
         json_docs = []
         single_doc = mycol.find().sort("code", -1)
         new_dict = mycol.find().sort("code", -1)
-        
         #for x in single_doc:
             #print(x)
         print("-------------------------------KAROL La Niña de las flores-5--------------------------------")
-
-       
         #https://stackoverflow.com/questions/19674311/json-serializing-mongodb
-
         for doc in new_dict:
             json_doc = json.dumps(doc, default=json_util.default)
             json_doc = json_doc.replace("$oid", "id")
@@ -53,21 +49,14 @@ class Conn():
             #print("---------------------------------------------------------------")
             #print(doc)
             json_docs.append(json_doc)
-        
         #to export https://www.w3schools.com/python/python_file_write.asp   
         print(json_docs)
         f = open("Demo.json", "w")
         f.write(str(json_docs))
-
         print("-------------------------------KAROL La Niña de las flores-6.1--------------------------------")
         with open('data1.json', 'w') as outfile:
           json.dump(json_docs, outfile, sort_keys = True, indent = 4,ensure_ascii = False)
-          
-
-
         print("-------------------------------KAROL La Niña de las flores-6--------------------------------")
-        
-        
         # Write JSON file
         with io.open('data.json', 'w', encoding='utf8') as outfile:
           str_ = json.dumps(json_docs,indent=4, sort_keys=True,separators=(',', ': '), ensure_ascii=False)
@@ -75,15 +64,10 @@ class Conn():
         #Check https://github.com/MartinThoma/mpu
         print("-------------------------------KAROL La Niña de las flores-7--------------------------------")
         print(str(str_))
-
         print("-------------------------------KAROL La Niña de las flores-8--------------------------------")
-
         with open('data.json', 'r') as filehandle:
          basiclist = json.load(filehandle)
          print(basiclist)
-
-
-
         #json_doc = json.dumps(single_doc,default=json_util.default)
         #json_doc = json_doc.replace("$oid", "id")
         #json_doc = json_doc.replace("_id", "uid")
@@ -91,10 +75,9 @@ class Conn():
         #print(json_doc)
         #return json.loads(str(json_doc))
 #-----------------------------------------------------------------------------------------------------------------------#
-         
     def closeConexion(self):
-          print ("Error on Closing")
-
+          print ("Error on Closing the Conection!!")
+    
     def getall(self):
            self = pymongo.MongoClient("mongodb+srv://userMongo4:123admina@cluster0-0njxn.mongodb.net/test?retryWrites=true")
            print(self.list_database_names())
